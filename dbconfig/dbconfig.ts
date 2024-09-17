3309d0dfefb8790910601fe84cc1bbfb060a8a5c
Khushi Sinha <ks1393990@gmail.com>
2024-09-17 10:50:47 +0530

    chore: Update MongoDB connection string in .env file and dbconfig.ts

--- dbconfig/dbconfig.ts
+++ dbconfig/dbconfig.ts
@@ -1,3 +1,20 @@
-import { connect } from "http2";
+
 import mongoose from "mongoose";
-export connect
+export async function connect(){
+    try{
+        mongoose.connect(process.env.MONGO_URI! )
+        const connection = mongoose.connection;
+
+        connection.once("connected", () => {
+            console.log("MongoDB Succesfully Connected")
+        })
+        connection.on("error", (err) => {
+            console.log("Error Connecting to database. Please make sure MongoDB is running. "+ err);
+            process.exit();
+        })
+    }
+    catch(error){
+        console.log("Error connecting to database");
+        console.log(error);
+    }
+} 
