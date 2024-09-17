55f571376b15fd4adc46c8c58a22f6312609e4f2
Khushi Sinha <ks1393990@gmail.com>
2024-09-18 00:33:27 +0530

    feat: Add login and signup pages
    This commit adds the login and signup pages to the application. The login page allows users to enter their email and password to log in, while the signup page allows users to create a new account by providing their username, email, and password. Both pages include form inputs and buttons for user interaction.
    
    The login page can be accessed at /login and the signup page can be accessed at /signup.
    
    This commit addresses the following user stories:
    - As a user, I want to be able to log in to the application.
    - As a user, I want to be able to create a new account.
    
    This commit also includes the following changes:
    - Import the necessary dependencies for the login and signup pages.
    - Define the structure and layout of the login and signup pages.
    - Implement form inputs and buttons for user interaction.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter } from "next/navigation";
+import {Axios} from "axios";
 
-const page = () => {
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  })
+  const onLogin = async () => {}
+
   return (
-    <div>login</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      
+      <label htmlFor = 'email'>email</label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            id= "email"
+            type= "text"
+            value= {user.email}
+            onChange= {(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+      />
+      <label htmlFor = 'password'>password</label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            id= "password"
+            type= "password"
+            value= {user.password}
+            onChange= {(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+      />
+      <button 
+       onClick= {onLogin}
+      className="p-2 bg-blue-500 text-white rounded-lg">Login</button>
+      <Link href="/signup">SignUp</Link>
+    </div>
   )
 }
-
-export default page
